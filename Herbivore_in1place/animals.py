8e0f82ee8671b7c5b88cf6f8436ee8000e8d0ea1
Jon-Fredrik Blakstad Cappelen <jon-fredrik.blakstad.cappelen@nmbu.no>
2018-01-08 14:32:51 +0100

    Lager en default-dictionary som settes inn i init, fremfor å skive rett i init.

--- Herbivore_in1place/animals.py
+++ Herbivore_in1place/animals.py
@@ -10,44 +10,26 @@ __email__ = 'jon-fredrik.blakstad.cappelen@nmbu.no'
 class Herbivore:
     """"""
 
-    def __init__(self, age, weight):
-        self.parameters = {'age': age,
-                           'weight': weight,
-                           'w_birth': 8.0,
-                           'sigma_birth': 1.5,
-                           'beta': 0.9,
-                           'eta': 0.05,
-                           'a_half': 40.0,
-                           'phi_age': 0.2,
-                           'w_half': 10.0,
-                           'phi_weight': 0.1,
-                           'mu': 0.25,
-                           'lambda': 1.0,
-                           'gamma': 0.2,
-                           'zeta': 3.5,
-                           'xi': 1.2,
-                           'omega': 0.4,
-                           'F': 10.0}
+    DEFAULT_PARAMETERS = {'w_birth': 8.0,
+                          'sigma_birth': 1.5,
+                          'beta': 0.9,
+                          'eta': 0.05,
+                          'a_half': 40.0,
+                          'phi_age': 0.2,
+                          'w_half': 10.0,
+                          'phi_weight': 0.1,
+                          'mu': 0.25,
+                          'lambda': 1.0,
+                          'gamma': 0.2,
+                          'zeta': 3.5,
+                          'xi': 1.2,
+                          'omega': 0.4,
+                          'F': 10.0}
 
-        """
-        self.age = age
-        self.weight = weight
-        self.w_birth = 8.0
-        self.sigma_birth = 1.5
-        self.beta = 0.9
-        self.eta = 0.05
-        self.a_half = 40.0
-        self.phi_age = 0.2
-        self.w_half = 10.0
-        self.phi_weight = 0.1
-        self.mu = 0.25
-        self.lambda = 1.0
-        self.gamma = 0.2
-        self.zeta = 3.5
-        self.xi = 1.2
-        self.omega = 0.4
-        self.F = 10.0
-        """
+    def __init__(self, age, weight, default_parameters=DEFAULT_PARAMETERS):
+        self.parameters = default_parameters
+        self.parameters['age'] = age
+        self.parameters['weight'] = weight
 
     def set_parameters(self, dictionary_changes):
         """"""
