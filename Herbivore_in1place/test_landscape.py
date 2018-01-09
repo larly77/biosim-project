5e273432978085454d6821f7a729d6429a4cd9af
Jon-Fredrik Blakstad Cappelen <jon-fredrik.blakstad.cappelen@nmbu.no>
2018-01-09 10:09:18 +0100

    Herbivore: Endra init for å unngå Mutables as default arguments. animals_test.py --> test_animals.py. Oppretter test_landscape.py og landscape.py

---
+++ Herbivore_in1place/test_landscape.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = 'Jon-Fredrik Blakstad Cappelen', 'Lars Martin Boe Lied'
+__email__ = 'jon-fredrik.blakstad.cappelen@nmbu.no',\
+            'lars.martin.boe.lied@nmbu.no'
