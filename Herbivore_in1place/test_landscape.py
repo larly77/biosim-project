0dcfb71c49be9ebabb42d36fbdf4fa226a95a90f
Lars Martin Lied <lars.martin.boe.lied@nmbu.no>
2018-01-09 14:35:16 +0100

    lagd tester til Jungle-metodene: get_fodder, reduce_fodder, reset_fodder

--- Herbivore_in1place/test_landscape.py
+++ Herbivore_in1place/test_landscape.py
@@ -7,5 +7,33 @@ __author__ = 'Jon-Fredrik Blakstad Cappelen', 'Lars Martin Boe Lied'
 __email__ = 'jon-fredrik.blakstad.cappelen@nmbu.no',\
             'lars.martin.boe.lied@nmbu.no'
 
+from landscape import Jungle
 
-def test_
+class TestLandscape:
+    """Class for testing landscape"""
+
+    def test_reset_fodder(self):
+        """Test for the method reset_fodder"""
+        j1 = Jungle()
+        j1.fodder = 500.0
+        j1.reset_fodder()
+        assert j1.fodder == 800.0
+
+    def test_reduce_fodder(self):
+        """Test for method reduce_fodder"""
+        j1 = Jungle()
+        j1.reduce_fodder(100.0)
+        assert j1.fodder == 700.0
+        j1.reduce_fodder(100.0)
+        assert j1.fodder == 600
+
+    def test_get_fodder(self):
+        """Test for metgod get_fodder"""
+        j1 = Jungle()
+        assert j1.get_fodder() == 800.0
+        j1.reduce_fodder(100)
+        assert j1.get_fodder() == 700
+
+
+
+
