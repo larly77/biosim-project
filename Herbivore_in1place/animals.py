701ff5e5b4fb3b1e4cf2b8384ae49034688e8a57
Jon-Fredrik Blakstad Cappelen <jon-fredrik.blakstad.cappelen@nmbu.no>
2018-01-09 11:12:01 +0100

    Lagt til default verdier for Carnivore og fikset over/under indent etter PEP-8.

--- Herbivore_in1place/animals.py
+++ Herbivore_in1place/animals.py
@@ -13,20 +13,36 @@ import math
 
 
 DEFAULT_HERBIVORE_PARAMETERS = {'w_birth': 8.0,
-                           'sigma_birth': 1.5,
-                           'beta': 0.9,
-                           'eta': 0.05,
-                           'a_half': 40.0,
-                           'phi_age': 0.2,
-                           'w_half': 10.0,
-                           'phi_weight': 0.1,
-                           'mu': 0.25,
-                           'lambda': 1.0,
-                           'gamma': 0.2,
-                           'zeta': 3.5,
-                           'xi': 1.2,
-                           'omega': 0.4,
-                           'F': 10.0}
+                                'sigma_birth': 1.5,
+                                'beta': 0.9,
+                                'eta': 0.05,
+                                'a_half': 40.0,
+                                'phi_age': 0.2,
+                                'w_half': 10.0,
+                                'phi_weight': 0.1,
+                                'mu': 0.25,
+                                'lambda': 1.0,
+                                'gamma': 0.2,
+                                'zeta': 3.5,
+                                'xi': 1.2,
+                                'omega': 0.4,
+                                'F': 10.0}
+DEFAULT_CARNIVORE_PARAMETERS = {'w_birth': 6.0,
+                                'sigma_birth': 1.0,
+                                'beta': 0.75,
+                                'eta': 0.125,
+                                'a_half': 60.0,
+                                'phi_age': 0.4,
+                                'w_half': 4.0,
+                                'phi_weight': 0.4,
+                                'mu': 0.4,
+                                'lambda': 1.0,
+                                'gamma': 0.8,
+                                'zeta': 3.5,
+                                'xi': 1.1,
+                                'omega': 0.9,
+                                'F': 50.0,
+                                'DeltaPhiMax': 10.0}
 
 
 class Herbivore:
@@ -50,9 +66,11 @@ class Herbivore:
     def update_fitness(self):
         """Method to update the fitness of the animal"""
         q_plus = 1/(1+math.exp(self.parameters['phi_age'] *
-                            (self.parameters['age']-self.parameters['a_half'])))
+                               (self.parameters['age'] -
+                                self.parameters['a_half'])))
         q_minus = 1/(1+math.exp(-self.parameters['phi_weight'] *
-                         (self.parameters['weight']-self.parameters['w_half'])))
+                                (self.parameters['weight'] -
+                                self.parameters['w_half'])))
 
         self.parameters['fitness'] = q_plus * q_minus
 
@@ -73,7 +91,7 @@ class Herbivore:
     def loss_of_weight(self):
         """Method that decreases the weight of the animal by a percent-value"""
         self.parameters['weight'] -= self.parameters['eta'] *\
-                                     self.parameters['weight']
+            self.parameters['weight']
         self.update_fitness()
 
     def death(self):
