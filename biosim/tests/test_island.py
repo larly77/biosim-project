d6dc2601b0320dbab2ca15c4ee47e253ac527947
Lars Martin Lied <lars.martin.boe.lied@nmbu.no>
2018-01-17 14:47:44 +0100

    lagt til en fixture for default verdier for dyra

--- biosim/tests/test_island.py
+++ biosim/tests/test_island.py
@@ -13,6 +13,39 @@ from biosim.animals import Carnivore, Herbivore
 import math
 import pytest
 
+DEFAULT_HERBIVORE_PARAMETERS = {'w_birth': 8.0,
+                                    'sigma_birth': 1.5,
+                                    'beta': 0.9,
+                                    'eta': 0.05,
+                                    'a_half': 40.0,
+                                    'phi_age': 0.2,
+                                    'w_half': 10.0,
+                                    'phi_weight': 0.1,
+                                    'mu': 0.25,
+                                    'lambda': 1.0,
+                                    'gamma': 0.2,
+                                    'zeta': 3.5,
+                                    'xi': 1.2,
+                                    'omega': 0.4,
+                                    'F': 10.0}
+
+DEFAULT_CARNIVORE_PARAMETERS = {'w_birth': 6.0,
+                                'sigma_birth': 1.0,
+                                'beta': 0.75,
+                                'eta': 0.125,
+                                'a_half': 60.0,
+                                'phi_age': 0.4,
+                                'w_half': 4.0,
+                                'phi_weight': 0.4,
+                                'mu': 0.4,
+                                'lambda': 1.0,
+                                'gamma': 0.8,
+                                'zeta': 3.5,
+                                'xi': 1.1,
+                                'omega': 0.9,
+                                'F': 50.0,
+                                'DeltaPhiMax': 10.0}
+
 ISLE_MAP = """\
         JSS
         SSJ
@@ -38,6 +71,11 @@ ISLE_MAP2 = """\
 
 class TestIsland:
     """Class for testing island"""
+
+    @pytest.fixture(autouse=True)
+    def set_default_animal(self):
+        Herbivore.set_parameters(DEFAULT_HERBIVORE_PARAMETERS)
+        Carnivore.set_parameters(DEFAULT_CARNIVORE_PARAMETERS)
 
     def test_string_to_array(self):
         """Test for the method Island.string_to_array"""
