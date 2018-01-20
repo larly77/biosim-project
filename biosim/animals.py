f452c08f99b8d3624ed642a63c33a479cb069a2f
Lars Martin Lied <lars.martin.boe.lied@nmbu.no>
2018-01-20 12:31:36 +0100

    lagde docstrings for animals

--- biosim/animals.py
+++ biosim/animals.py
@@ -66,13 +66,28 @@ class Herbivore:
             cls.parameters[key] = parameter_changes[key]
 
     def __init__(self, age, weight):
+        """
+        init-function for setting age and weight to a herbivore
+        Parameters
+        ----------
+        age : int
+            age of herbivore
+        weight : float
+            weight of herbivore
+        """
         self.age = age
         self.weight = weight
         self.fitness = None
         self.update_fitness()
 
     def update_fitness(self):
-        """Method to update the fitness of the animal"""
+        """
+        Method for updating fitness
+        Returns
+        -------
+        An updated value of fitness
+
+        """
         q_plus = 1/(1+math.exp(self.parameters['phi_age'] *
                                (self.age -
                                 self.parameters['a_half'])))
@@ -83,12 +98,25 @@ class Herbivore:
         self.fitness = q_plus * q_minus
 
     def get_weight(self):
-        """Returns the weight of the animal"""
+        """
+        Returns
+        -------
+        returns the weight of the animal
+
+        """
         return self.weight
 
     def feeding(self, landscape_instance):
-        """Dummy"""
+        """
+        Handles the feeding of  the animal
+        Parameters
+        ----------
+        landscape_instance : The tile, that the given animal is in
 
+        Returns
+        -------
+
+        """
         available_fodder = landscape_instance.get_fodder()
         appetite = self.parameters['F']
         if available_fodder >= appetite:
@@ -103,7 +131,19 @@ class Herbivore:
             pass
 
     def procreation(self, landscape_instance, number_of_adults):
-        """Dummy"""
+        """
+        Handles the procreation of the animals
+        Parameters
+        ----------
+        landscape_instance : object
+            The tile of the animal
+        number_of_adults : int
+            Number of animals old enough to procreate
+
+        Returns
+        -------
+
+        """
         if self.weight >= self.parameters['zeta'] * (
                 self.parameters['w_birth'] + self.parameters['sigma_birth']):
             probability_of_birth = min([1, self.parameters['gamma'] *
@@ -122,29 +162,57 @@ class Herbivore:
                         Carnivore(age=0, weight=weight_birth))
 
     def migration(self):
-        """Dummy"""
+        """
+        Method for checking if the animal will migrate
+        Returns
+        -------
+        True or False value, wether the animal wil move or not
+
+        """
         probability_move = self.parameters['mu'] * self.fitness
         return random.random() < probability_move
 
     def aging(self):
+        """
+        Method for handling the aging of an animal, also update fitness
+        Returns
+        -------
+
+        """
         """Method that increases the age of the animal by one year"""
         self.age += 1
         self.update_fitness()
 
     def loss_of_weight(self):
+        """
+        Method for handling the loss of weight, by natural causes
+
+        A mathod for decreasing the weight of animal every year by a parameter
+        'eta' multiplied by the animals own weight, also updates fitness
+        Returns
+        -------
+
+        """
         """Method that decreases the weight of the animal by a percent-value"""
         self.weight -= self.parameters['eta'] *\
             self.weight
         self.update_fitness()
 
     def death(self):
-        """Dummy"""
+        """
+        Method for for checking if the animal should die from natural causes
+
+        Calculates the chance of dying due to low fitness, by using the
+        parameter 'omega'
+        Returns
+        -------
+
+        """
         probability_of_death = self.parameters['omega'] * (1-self.fitness)
         return random.random() < probability_of_death
 
 
 class Carnivore(Herbivore):
-    """"""
 
     DEFAULT_CARNIVORE_PARAMETERS = {'w_birth': 6.0,
                                     'sigma_birth': 1.0,
@@ -165,10 +233,40 @@ class Carnivore(Herbivore):
     parameters = copy.deepcopy(DEFAULT_CARNIVORE_PARAMETERS)
 
     def __init__(self, age, weight):
+        """
+        Init function for carnivores
+
+        The carnivore class is a subclass of the Herbivore class, using most of
+        the methods from Herbivore except the feeding method
+        Parameters
+        ----------
+        age : int
+            Age of animal
+        weight : float
+            Weight of animal
+        """
         super().__init__(age, weight)
 
     def feeding(self, preys):
-        """"""
+        """
+        Feeding method for carnivores
+
+        Goes though all the herbivores in the same tile as the carnivore
+        and checks if the carnivore eats it or not, depending on fitness of
+        both carnivore an herbivore, the parameters 'DeltaPhiMax' and 'beta and
+        the appetite of the carnivore and a bit of luck.
+        It also updates the weight and fitness of the carnivore.
+        Parameters
+        ----------
+        preys : list
+            A list of all the herbivores on the same tile as the given carnovre
+
+        Returns
+        -------
+        eaten_bool : list
+            Liste of true/false values for which herbivores to be eaten
+
+        """
         appetite = copy.deepcopy(self.parameters['F'])
         eaten_bool = [True] * len(preys)
         for index, prey in enumerate(preys):
