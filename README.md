d079603b85e9bdd3ab16be2d48298f130517cb3c
Lars Martin Lied <lars.martin.boe.lied@nmbu.no>
2018-01-23 16:41:13 +0100

    fiksa på setup.py of readme

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Lars Martin Boe Lied, NMBU
 	lars.martin.boe.lied@nmbu.no
 
 This is a program for simulating an island with herbivores and carnivores
+
+Contents
+---------
+
+ - biosim: Python package for simulation of an island ecosystem
+ - examples: Scripts for illustrating the use of the package
--- biosim/setup.py
+++ biosim/setup.py
@@ -2,30 +2,34 @@ from setuptools import setup
 import codecs
 import os
 
+
 def read_readme():
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
         long_description = f.read()
     return long_description
 
+
 setup(
       # Basic information
       name='biosim',
-      version='0.1.0',
+      version='0.1.1',
 
       # Packages to include
       packages=['biosim', 'biosim.tests'],
 
       # Required packages not included in Python standard library
-      requires=['pytest', 'numpy', 'matplotlib', 'pandas', 'textwrap', 'random', 'copy', 'math'],
+      requires=['pytest (>=3.0.7)', 'numpy (>=1.12.1)', 'matplotlib (>=2.0.2)',
+                'pandas (>=0.20.1)'],
 
       # Metadata
       description='A Herbivores and Carnivores Simulation',
       long_description=read_readme(),
       author='Lars Martin Lied, Jon-Fredrik Cappelen, NMBU',
-      author_email='lars.martin.boe.lied@nmbu.no, jon-fredrik.blakstad.cappelen@nmbu.no',
+      author_email='lars.martin.boe.lied@nmbu.no, '
+                   'jon-fredrik.blakstad.cappelen@nmbu.no',
       url='https://bitbucket.org/g04_jon_lars/biosim_g04_jon_lars',
-      keywords='simulation game',
+      keywords='simulation ecosystem',
       license='MIT License',
       classifiers=[
         'Development Status :: 3 - Alpha',
