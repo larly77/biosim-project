d7e4f764f461d3128b33c68402eba2e5098a08cd
Lars Martin Lied <lars.martin.boe.lied@nmbu.no>
2018-01-23 16:06:04 +0100

    __init__ file for biosim and biosim.tests

---
+++ biosim/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for Python package for BioSim
+"""
+
+__author__ = 'Jon-Fredrik Blakstad Cappelen', 'Lars Martin Boe Lied'
+__email__ = 'jon-fredrik.blakstad.cappelen@nmbu.no',\
+            'lars.martin.boe.lied@nmbu.no'
